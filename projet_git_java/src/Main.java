e6f073ad2915fcf81967aed8e1b271e8729f89e2
maryem <maryem.baldi@esprit.tn>
2023-10-13 10:22:16 +0100

    maryem2

--- projet_git_java/src/Main.java
+++ projet_git_java/src/Main.java
@@ -1,17 +1,68 @@
-// Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
-// then press Enter. You can now see whitespace characters in your code.
 import java.util.Arrays;
+
 public class Main {
     public static void main(String[] args) {
 
-        int[] tab = {5, 1, 8, 0, 9, 4};
+        System.out.println("bla bla test 1");
 
-        Arrays.sort(tab);
+        int[] tab2 = {1, 2, 8, 4, 5};
 
-        System.out.printf("tab[] : %s", Arrays.toString(tab));;
+        // Calculer la moyenne
+        float moyenne = calculerMoyenne(tab2);
+        System.out.println("La moyenne des éléments du tableau est : " + moyenne);
 
+        // Trier le tableau
+        trierTableau(tab2);
+        System.out.println("Tableau trié : " + Arrays.toString(tab2));
 
+        // Calculer le produit des éléments
+        int produit = calculerProduit(tab2);
+        System.out.println("Le produit des éléments du tableau est : " + produit);
+
+        // Calculer la somme des éléments
+        int somme = calculerSomme(tab2);
+        System.out.println("La somme des éléments du tableau est : " + somme);
+
+        // Trouver la valeur maximale
+        int max = trouverMaximum(tab2);
+        System.out.println("La valeur maximale du tableau est : " + max);
     }
-
-
+    // calculer la moyenne
+    public static float calculerMoyenne(int[] tableau) {
+        int somme = 0;
+        for (int i : tableau) {
+            somme += i;
+        }
+        return (float) somme / tableau.length;
+    }
+    // Fonction pour trier le tableau
+    public static void trierTableau(int[] tableau) {
+        Arrays.sort(tableau);
+    }
+    // Fonction pour calculer le produit des éléments
+    public static int calculerProduit(int[] tableau) {
+        int produit = 1;
+        for (int i : tableau) {
+            produit *= i;
+        }
+        return produit;
+    }
+    // Fonction pour calculer la somme des éléments
+    public static int calculerSomme(int[] tableau) {
+        int somme = 0;
+        for (int i : tableau) {
+            somme += i;
+        }
+        return somme;
+    }
+    // Fonction pour trouver la valeur maximale
+    public static int trouverMaximum(int[] tableau) {
+        int max = tableau[0];
+        for (int i : tableau) {
+            if (i > max) {
+                max = i;
+            }
+        }
+        return max;
+    }
 }
